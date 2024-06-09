fb72a7c86a856c83737ecfda1ac0c49609545f1a
issa2580 <issadiop2580@gmail.com>
2024-06-09 00:38:31 +0000

    improve server

--- server.js
+++ server.js
@@ -39,9 +39,9 @@ app.get("/", function (req, res) {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
 
